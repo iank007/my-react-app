57f0901e7d2261375014857d3d832d8ef2927f71
januar <dream.muhtadiin@gmail.com>
2025-03-18 10:51:17 +0700

    add component Teacher

---
+++ src/components/Teacher.jsx
@@ -0,0 +1,13 @@
+export const Teacher = () => {
+
+    return (
+        <div className="card">
+            <h2>Teacher Januar</h2>
+            <div>
+                <button type='button'>Join ReactJS</button>
+                &nbsp;
+                <button type='button'>Join Tahfidz</button>
+            </div>
+        </div>
+    )
+}
