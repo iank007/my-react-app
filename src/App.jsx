b8b54196d0c53d004a458a216eb79520b157edce
januar <dream.muhtadiin@gmail.com>
2025-03-19 05:27:56 +0700

    add component Student and show list of teacher-student

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useEffect, useState } from 'react';
 import './App.css'
 import Student from './components/Student'
 import { Teacher } from './components/Teacher'
 
 function App() {
+
   return (
     <>
-      <Welcome />
-      <Hook />
-      
+      <div>
+        <h1>Unattanded</h1>
+        <Teacher />
+        <Student />
+        <Student />
+        <Student />
+      </div>
+      <div>
+        <h1>Class ReactJS</h1>
+        <Teacher />
+        <Student />
+        <Student />
+        <Student />
+      </div>
+      <div>
+        <h1>Class Tahfidz</h1>
+        <Teacher />
+        <Student />
+        <Student />
+        <Student />
+        </div>
+      <div>
+        <h1>Other Class</h1>
+        <p><i>No class for today!</i></p>
+      </div>
     </>
   )
 }
---
+++ src/components/Student.jsx
@@ -0,0 +1,16 @@
+function Student() {
+
+    return (
+        <div className="card">
+            <h3>Student A</h3>
+            <p>no Seat: 1</p>
+            <div>
+                <button type='button'>Join ReactJS</button>
+                &nbsp;
+                <button type='button'>Join Tahfidz</button>
+            </div>
+        </div>
+    )
+}
+
+export default Student
